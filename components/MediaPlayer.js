1e18d36d34b8e2e59de7a540304224478532c760
luxexry <luxexry@gmail.com>
2024-06-25 14:11:02 -0400

    Fix hydration error

--- components/MediaPlayer.js
+++ components/MediaPlayer.js
@@ -1,29 +1,40 @@
 // components/MediaPlayer.js
-import React from 'react';
-import ReactPlayer from 'react-player';
+import { useEffect, useRef } from 'react';
+import videojs from 'video.js';
+import 'video.js/dist/video-js.css';
 
 const MediaPlayer = ({ url }) => {
+  const videoRef = useRef(null);
+  const playerRef = useRef(null);
+
+  useEffect(() => {
+    // Make sure Video.js is only initialized once
+    if (!playerRef.current) {
+      const videoElement = videoRef.current;
+      if (!videoElement) return;
+
+      playerRef.current = videojs(videoElement, {
+        controls: true,
+        autoplay: true,
+        preload: 'auto',
+      });
+    } else {
+      // Update the player source
+      playerRef.current.src({ src: url, type: 'application/x-mpegURL' });
+    }
+
+    // Cleanup function to dispose the video.js player
+    return () => {
+      if (playerRef.current) {
+        playerRef.current.dispose();
+        playerRef.current = null;
+      }
+    };
+  }, [url]);
+
   return (
-    <div className="player-wrapper">
-      <ReactPlayer
-        className="react-player"
-        url={url}
-        playing
-        controls
-        width="100%"
-        height="100%"
-      />
-      <style jsx>{`
-        .player-wrapper {
-          position: relative;
-          padding-top: 56.25%; /* 16:9 aspect ratio */
-        }
-        .react-player {
-          position: absolute;
-          top: 0;
-          left: 0;
-        }
-      `}</style>
+    <div data-vjs-player>
+      <video ref={videoRef} className="video-js vjs-big-play-centered" />
     </div>
   );
 };
